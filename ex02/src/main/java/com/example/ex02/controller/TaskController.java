f3edd1142cdc40bbf7cdc936e32baa0de2fd505c
Ricardokim94 <cmrikim@naver.com>
2023-01-22 15:53:11 +0900

    admin페이지 id controller에서 분기처리

--- ex02/src/main/java/com/example/ex02/controller/TaskController.java
+++ ex02/src/main/java/com/example/ex02/controller/TaskController.java
@@ -2,15 +2,16 @@ package com.example.ex02.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class TaskController {
 //아이디와 비밀번호를 입력받은 후 아이디가 admin일 경우 admin.html로 이동 
-//아이디가 user 일 경우 user.html로 이동 
+//아이디가 user 일 경우 user.jsp로 이동 
 	
-	//-amdin.html
-	//user.html
+	//-amdin.jsp
+	//user.jsp
 			
 //id와 pw를 입력할 수 있는 화면이 필요! + 화면으로 이동 할 수있는 method 필요!
 	@GetMapping("/loginForm")
@@ -18,12 +19,16 @@ public class TaskController {
 		return "task/task01/login";
 	}
 	
+	//버튼을 누르면 넘어가는 페이지 
 	@PostMapping("/login")
-	public String login(String id , String pw) {
-		//admin이 아니면 싹다 가라!
-		if(id.equals("user")) {
-			return;
+	//외부에서 전달받은 id와 pw를 매개변수로 받는다.
+	public String login(@ModelAttribute("id") String id , String pw) { //@ModelAttribute : 전달할페이지 에다가 id란 값을 전달한다.
+		//만약 id 가 admin일 경우 admin.jsp로 이동 시킨다.
+		if(id.equals("admin")) {
+			return "task/task01/admin";
 		}
+		//만약 id가 admin아닐 경우 user.jsp로 넘어가라  
+		return "task/task01/user";
 	}
 	
 }
--- ex02/src/main/webapp/WEB-INF/views/task/task01/login.jsp
+++ ex02/src/main/webapp/WEB-INF/views/task/task01/login.jsp
@@ -8,7 +8,7 @@
 <title>Login Page </title>
 </head>
 <body>
-	<form action="" method="post">
+	<form action="/login" method="post">
 		<input type="text" name="id" placeholder="아이디를 입력하세요.">
 		<input type="password" name="pw" placeholder="비밀번호를 입력하세요.">
 		<button>로그인</button>
